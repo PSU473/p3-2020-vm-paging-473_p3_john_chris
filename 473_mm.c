8fad12817e04740edac8741dfb2c25eabae05d9a
vishwasvk <56511904+codemonk98@users.noreply.github.com>
2020-11-18 12:57:25 -0500

    Initial commit

---
+++ 473_mm.c
@@ -0,0 +1,11 @@
+// Starter code for the page replacement project
+#define _GNU_SOURCE 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/mman.h>
+#include <signal.h>
+#include "473_mm.h"
+
+void mm_init(void* vm, int vm_size, int n_frames, int page_size, int policy)
+{}
